916e44ac319c08f40050935c8112609444c90c8c
Konstantin Larionov <konstantin.l@octopays.io>
2025-09-22 21:17:13 +0700

    ~~

---
+++ index.js
@@ -0,0 +1,26 @@
+﻿import { createServer } from "http";
+import { Server } from "socket.io";
+
+const httpServer = createServer();
+const io = new Server(httpServer, { cors: { origin: "*" } });
+
+io.on("connection", socket => {
+    socket.on("join", room => {
+        socket.join(room);
+        socket.to(room).emit("new-user", socket.id);
+    });
+
+    socket.on("offer", (data) => {
+        io.to(data.to).emit("offer", { from: socket.id, sdp: data.sdp });
+    });
+
+    socket.on("answer", (data) => {
+        io.to(data.to).emit("answer", { from: socket.id, sdp: data.sdp });
+    });
+
+    socket.on("candidate", (data) => {
+        io.to(data.to).emit("candidate", { from: socket.id, candidate: data.candidate });
+    });
+});
+
+httpServer.listen(3000, () => console.log("Signaling server started"));
